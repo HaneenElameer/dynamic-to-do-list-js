13ddf69bb472cd522747f7bc88e037c8d1c6f9d8
Haneen Elameer <Haneenelameer23@gmail.com>
2024-10-26 18:47:51 +0300

    local storage

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 document.addEventListener('DOMContentLoaded',function(){
+
     const addButton = document.getElementById('add-task-btn')
     const taskInput = document.getElementById('task-input')
     const taskList = document.getElementById('task-list')
-    function addTask(){
+    
+
+    function addTask (){
         const taskText = taskInput.value.trim()
-        if (taskText ==="" ){
-            alert("please enter a task")
+
+        if (taskText){
+            createNewElement(taskText)
+            taskInput.value = ""
+            saveTasks()
         }else{
-            const newTask = document.createElement('li')
-            newTask.textContent = taskText
-            taskList.appendChild(newTask)
-            taskInput.value='';
-            const deleteBtn = document.createElement('button')
-            deleteBtn.textContent = 'Remove'
-            deleteBtn.classList.add = 'remove-btn'
-            newTask.appendChild(deleteBtn)
-            deleteBtn.addEventListener('click',function(){
-                taskList.removeChild(newTask)
-            })
+            alert('please enter a task.')
         }
     }
+
     addButton.addEventListener('click',addTask)
     taskInput.addEventListener('keypress',function(event){
         if (event.key === 'Enter'){
             addTask()
         }
     })
-    addTask()
+
+
+
+    function createNewElement(taskText){
+        const newTask = document.createElement('li')
+        newTask.textContent = taskText
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'Remove'
+        deleteBtn.classList.add ('remove-btn')
+
+        taskList.appendChild(newTask)
+        newTask.appendChild(deleteBtn)
+        deleteBtn.addEventListener('click',function(){
+            taskList.removeChild(newTask)
+            saveTasks( )
+        } )
+
+
+        
+    }
+
+
+    function saveTasks( ){
+        let tasks = []
+        taskList.querySelectorAll('li').forEach(function(item){
+            tasks.push(item.textContent.replace('Remove',"").trim())
+        })
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+
+    }
+
+    function loadTasks(){
+        const tasks = JSON.parse(
+            localStorage.getItem('tasks')) || []
+       tasks.forEach(createNewElement)
+    }
+    loadTasks()
 })
